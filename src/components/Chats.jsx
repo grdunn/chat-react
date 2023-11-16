db305c0699db87b996169524c5daaa5787502081
Greg Dunn <grdunn@gmail.com>
2023-11-16 11:46:17 -0500

    search for chats. fetch chats on load

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import { doc, onSnapshot } from "firebase/firestore";
+import React, { useContext, useEffect, useState } from "react";
+import { db } from "../firebase";
+import { AuthContext } from "../context/AuthContext";
 
 const Chats = () => {
-  return <div className="chats">Chats</div>;
+  const [chats, setChats] = useState([]);
+  const { currentUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    const getChats = () => {
+      const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
+        setChats(doc.data());
+      });
+
+      return () => {
+        unsub();
+      };
+    };
+    currentUser.uid && getChats();
+  }, [currentUser.uid]);
+
+  Object.entries(chats)?.map((chat) => {
+    console.log(chat);
+  });
+
+  return (
+    <div className="chats">
+      <h3>Chats</h3>
+      {Object.entries(chats)?.map((chat) => {
+        return (
+          <div key={chat[0]}>
+            <span>{chat[1].userInfo.displayName}</span>
+            <span>{chat[1].lastMessage?.text}</span>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Chats;
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,23 @@
-import React, { useState } from "react";
-import { collection, query, where, getDocs } from "firebase/firestore";
+import React, { useState, useContext } from "react";
+import {
+  collection,
+  query,
+  where,
+  getDocs,
+  doc,
+  getDoc,
+  setDoc,
+  updateDoc,
+  serverTimestamp,
+} from "firebase/firestore";
 import { db } from "../firebase";
+import { AuthContext } from "../context/AuthContext";
 
 const Search = () => {
   const [username, setUsername] = useState("");
   const [user, setUser] = useState(null);
   const [err, setErr] = useState(false);
+  const { currentUser } = useContext(AuthContext);
 
   const handleSearch = async () => {
     const q = query(
@@ -15,7 +27,11 @@ const Search = () => {
 
     try {
       const querySnapshot = await getDocs(q);
-      setUser(doc.data());
+      // TODO: If no user is found, remove the previous user.
+      querySnapshot.forEach((doc) => {
+        setUser(doc.data());
+        // is !res.exists()
+      });
     } catch (err) {
       setErr(err);
     }
@@ -25,7 +41,43 @@ const Search = () => {
     e.code === "Enter" && handleSearch();
   };
 
-  const handleSelect = async () => {};
+  const handleSelect = async () => {
+    const combinedId =
+      currentUser.uid > user.uid
+        ? currentUser.uid + user.uid
+        : user.uid + currentUser.uid;
+
+    try {
+      const res = await getDoc(doc(db, "chats", combinedId));
+
+      if (!res.exists()) {
+        // create a chat in chats collections
+        await setDoc(doc(db, "chats", combinedId), { messages: [] });
+
+        await updateDoc(doc(db, "userChats", currentUser.uid), {
+          [combinedId + ".userInfo"]: {
+            uid: user.uid,
+            displayName: user.displayName,
+            photoURL: user.photoURL,
+          },
+          [combinedId + ".date"]: serverTimestamp(),
+        });
+
+        await updateDoc(doc(db, "userChats", user.uid), {
+          [combinedId + ".userInfo"]: {
+            uid: currentUser.uid,
+            displayName: currentUser.displayName,
+            photoURL: currentUser.photoURL,
+          },
+          [combinedId + ".date"]: serverTimestamp(),
+        });
+      }
+    } catch (err) {
+      setErr(err);
+    }
+    setUser(null);
+    setUsername("");
+  };
 
   return (
     <div className="search">
@@ -34,8 +86,9 @@ const Search = () => {
         placeholder="Find a user"
         onKeyDown={handleKey}
         onChange={(e) => setUsername(e.target.value)}
+        value={username}
       />
-      {err && <span>{err}</span>}
+      {err && <span>{err.message}</span>}
       {user && (
         <div>
           <button onClick={handleSelect}>{user.displayName}</button>

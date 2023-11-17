511072c7f9662f72f837bc359d6d10d8509d517a
Greg Dunn <grdunn@gmail.com>
2023-11-16 21:45:03 -0500

    refactor for react

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -21,17 +21,75 @@ export const Login = () => {
   };
 
   return (
-    <div className="form-container">
-      <div className="form-wrapper">
-        <span>Login</span>
-        <form onSubmit={handleSubmit}>
-          <input type="email" placeholder="email" />
-          <input type="password" placeholder="password" />
-          <button>Sign In</button>
-        </form>
-        <p>
-          Do you need to sign up for an account?{" "}
-          <Link to="/register">Register</Link>
+    <div className="bg-slate-50 h-screen w-full px-6 form flex justify-center items-center">
+      <div className="mx-auto w-full  md:max-w-md">
+        <h2 className="text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          Log in to your account.
+        </h2>
+        <div className="mt-10 mx-auto w-full bg-white rounded-xl shadow p-10">
+          <form onSubmit={handleSubmit}>
+            <div className="space-y-6">
+              <div>
+                <label
+                  for="email"
+                  class="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Email
+                </label>
+                <div class="mt-2">
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-100 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+              <div>
+                <div class="flex items-center justify-between">
+                  <label
+                    for="password"
+                    class="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    Password
+                  </label>
+                  <div class="text-sm">
+                    <a
+                      href="#"
+                      class="font-semibold text-indigo-600 hover:text-indigo-500"
+                    >
+                      Forgot password?
+                    </a>
+                  </div>
+                </div>
+                <div class="mt-2">
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-100 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+              <div>
+                <button
+                  type="submit"
+                  class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Login
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+        <p class="mt-10 text-center text-sm text-gray-500">
+          Need to set up a new account?
+          <Link
+            className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500 ml-2"
+            to="/register"
+          >
+            Register
+          </Link>
         </p>
       </div>
     </div>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -77,19 +77,19 @@ export const Register = () => {
     <div className="bg-slate-50 h-screen w-full px-6 form flex justify-center items-center">
       <div className="mx-auto w-full  md:max-w-md">
         <h2 className="text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-          Register your account
+          Register your account.
         </h2>
         <div className="mt-10 mx-auto w-full bg-white rounded-xl shadow p-10">
           <form onSubmit={handleSubmit}>
             <div className="space-y-6">
               <div>
                 <label
-                  for="displayName"
-                  class="block text-sm font-medium leading-6 text-gray-900"
+                  htmlFor="displayName"
+                  className="block text-sm font-medium leading-6 text-gray-900"
                 >
                   Display name
                 </label>
-                <div class="mt-2">
+                <div className="mt-2">
                   <input
                     id="displayName"
                     name="displayName"
@@ -100,12 +100,12 @@ export const Register = () => {
               </div>
               <div>
                 <label
-                  for="email"
-                  class="block text-sm font-medium leading-6 text-gray-900"
+                  htmlFor="email"
+                  className="block text-sm font-medium leading-6 text-gray-900"
                 >
                   Email
                 </label>
-                <div class="mt-2">
+                <div className="mt-2">
                   <input
                     id="email"
                     name="email"
@@ -115,23 +115,15 @@ export const Register = () => {
                 </div>
               </div>
               <div>
-                <div class="flex items-center justify-between">
+                <div className="flex items-center justify-between">
                   <label
-                    for="password"
-                    class="block text-sm font-medium leading-6 text-gray-900"
+                    htmlFor="password"
+                    className="block text-sm font-medium leading-6 text-gray-900"
                   >
                     Password
                   </label>
-                  <div class="text-sm">
-                    <a
-                      href="#"
-                      class="font-semibold text-indigo-600 hover:text-indigo-500"
-                    >
-                      Forgot password?
-                    </a>
-                  </div>
                 </div>
-                <div class="mt-2">
+                <div className="mt-2">
                   <input
                     id="password"
                     name="password"
@@ -141,42 +133,51 @@ export const Register = () => {
                 </div>
               </div>
 
-              <div class="flex items-center justify-center w-full">
-                <label
-                  for="dropzone-file"
-                  class="flex flex-col items-center justify-center w-full h-40 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+              <div>
+                <span
+                  htmlFor="email"
+                  className="block text-sm font-medium leading-6 text-gray-900"
                 >
-                  <div class="flex flex-col items-center justify-center pt-5 pb-6">
-                    <svg
-                      class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
-                      aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 20 16"
-                    >
-                      <path
-                        stroke="currentColor"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
-                      />
-                    </svg>
-                    <p class="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                      <span class="font-semibold">Click to upload</span> or drag
-                      and drop
-                    </p>
-                    <p class="text-xs text-gray-500 dark:text-gray-400">
-                      SVG, PNG, JPG or GIF (MAX. 800x400px)
-                    </p>
-                  </div>
-                  <input id="dropzone-file" type="file" class="hidden" />
-                </label>
+                  Profile picture
+                </span>
+                <div className="mt-2 flex items-center justify-center w-full">
+                  <label
+                    htmlFor="dropzone-file"
+                    className="flex flex-col items-center justify-center w-full h-40 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+                  >
+                    <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                      <svg
+                        className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 20 16"
+                      >
+                        <path
+                          stroke="currentColor"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+                        />
+                      </svg>
+                      <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                        <span className="font-semibold">Click to upload</span>{" "}
+                        or drag and drop
+                      </p>
+                      <p className="text-xs text-gray-500 dark:text-gray-400">
+                        SVG, PNG, JPG or GIF (MAX. 800x400px)
+                      </p>
+                    </div>
+                    <input id="dropzone-file" type="file" className="hidden" />
+                  </label>
+                </div>
               </div>
+
               <div>
                 <button
                   type="submit"
-                  class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
                   Sign up
                 </button>
@@ -184,13 +185,13 @@ export const Register = () => {
             </div>
           </form>
         </div>
-        <p class="mt-10 text-center text-sm text-gray-500">
+        <p className="mt-10 text-center text-sm text-gray-500">
           Already have an account?
           <Link
             className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500 ml-2"
             to="/login"
           >
-            Login
+            Log in.
           </Link>
         </p>
       </div>
